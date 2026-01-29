dd036cad551fdb1d1b4e0f1441d55d2667b85771
Krish Singh <154358180+krishsingh120@users.noreply.github.com>
2026-01-29 10:29:23 +0530

    Fix: clean up the code

--- JavaRevision/RevArrRecursion.java
+++ JavaRevision/RevArrRecursion.java
@@ -15,11 +15,11 @@ public class RevArrRecursion {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Take input size of array");
+    System.out.println("Take input size of array N: ");
     int n = sc.nextInt();
     int arr[] = new int[n];
 
-    System.out.println("Take input array");
+    System.out.println("Take input array : ");
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
