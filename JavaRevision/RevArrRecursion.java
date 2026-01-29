2fd1ff5a4848ed3888801a8986fc4542f16f3dd5
krishsingh10386@gmail.com <bt23eci047@iiit.ac.in>
2026-01-29 10:21:02 +0530

    Feat: added reverse arr using recursion

---
+++ JavaRevision/RevArrRecursion.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class RevArrRecursion {
+
+  public static void reverse(int[] arr, int start, int end) {
+    if (start > end)
+      return;
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    reverse(arr, start + 1, end - 1);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Take input size of array");
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+
+    System.out.println("Take input array");
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    reverse(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+}
